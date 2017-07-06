f1f9a2daf5e6af9c0422ddb347f9773c23cc09f0
Pedro Scarpioni <pedroscarpioni@hotmail.com>
2017-07-06 20:41:03 -0300

    Simple site of history

---
+++ historia/js/album.js
@@ -0,0 +1,104 @@
+//Função que troca a imagem de acordo com o índice (variável 'numero') que recebe.
+function imagem(numero){
+	document.getElementById("myImage").src = "img/"+numero+".jpg";
+}
+
+//Função que faz slideshow com as imagens
+function slideShow(){
+
+	
+	//Depois de um segundo, coloca a imagem como '2.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/2.jpg";
+	},2000);
+
+	//Depois de dois segundos, coloca a imagem como '3.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/3.jpg";
+	},4000);
+
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/4.jpg";
+	},6000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/5.jpg";
+	},8000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/6.jpg";
+	},10000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/7.jpg";
+	},12000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/8.jpg";
+	},14000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/9.jpg";
+	},16000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/10.jpg";
+	},18000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/11.jpg";
+	},20000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/12.jpg";
+	},22000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/13.jpg";
+	},24000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/14.jpg";
+	},26000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/15.jpg";
+	},28000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/16.jpg";
+	},30000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/17.jpg";
+	},32000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/18.jpg";
+	},34000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/19.jpg";
+	},36000);
+	
+	//Depois de três segundos, coloca a imagem como '1.png'
+	setTimeout(function(){ 
+		document.getElementById("myImage").src = "img/primeira_guerra/20.jpg";
+	},38000);
+}
